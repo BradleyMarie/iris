bf726643445bdcc20fd17c8f461b553749513f82
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2022-08-14 23:09:17 -0700

    Set geometry on hits

---
+++ iris/geometry.cc
@@ -0,0 +1,25 @@
+#include "iris/geometry.h"
+
+#include "iris/internal/hit.h"
+
+namespace iris {
+
+Hit* Geometry::Trace(HitAllocator& hit_allocator,
+                     std::optional<geometric_t> minimum_distance,
+                     std::optional<geometric_t> maximum_distance) const {
+  auto* result = Trace(hit_allocator.ray_, hit_allocator, minimum_distance,
+                       maximum_distance);
+
+  auto* hit_list = result;
+  while (hit_list) {
+    auto* full_hit = static_cast<internal::Hit*>(hit_list);
+    if (!full_hit->geometry) {
+      full_hit->geometry = this;
+    }
+    hit_list = hit_list->next;
+  }
+
+  return result;
+}
+
+}  // namespace iris
--- iris/geometry.h
+++ iris/geometry.h
@@ -13,10 +13,7 @@ class Geometry {
  public:
   Hit* Trace(HitAllocator& hit_allocator,
              std::optional<geometric_t> minimum_distance = std::nullopt,
-             std::optional<geometric_t> maximum_distance = std::nullopt) const {
-    return Trace(hit_allocator.ray_, hit_allocator, minimum_distance,
-                 maximum_distance);
-  }
+             std::optional<geometric_t> maximum_distance = std::nullopt) const;
 
  private:
   virtual Hit* Trace(const Ray& ray, HitAllocator& hit_allocator,
