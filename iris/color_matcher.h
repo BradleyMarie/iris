44066916bd5a62bfba1e80f14c20fbbc1f33480e
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2022-09-09 15:49:50 -0700

    Add ColorMatcher class

---
+++ iris/color_matcher.h
@@ -0,0 +1,20 @@
+#ifndef _IRIS_COLOR_MATCHER_
+#define _IRIS_COLOR_MATCHER_
+
+#include <array>
+
+#include "iris/color.h"
+#include "iris/spectrum.h"
+
+namespace iris {
+
+class ColorMatcher {
+ public:
+  virtual std::array<visual_t, 3> Match(const Spectrum& spectrum) const = 0;
+  virtual Color::Space ColorSpace() const = 0;
+  virtual ~ColorMatcher() {}
+};
+
+}  // namespace iris
+
+#endif  // _IRIS_COLOR_MATCHER_
