45fa68362539e3b9cfa4dcf508823d7c33823a2c
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2022-09-15 00:45:52 -0700

    Define BSDF functions

--- iris/bsdf.h
+++ iris/bsdf.h
@@ -1,11 +1,52 @@
 #ifndef _IRIS_BSDF_
 #define _IRIS_BSDF_
 
+#include <optional>
+
+#include "iris/float.h"
+#include "iris/random.h"
 #include "iris/reflector.h"
+#include "iris/spectral_allocator.h"
+#include "iris/vector.h"
 
 namespace iris {
 
-class Bsdf;
+class Bsdf {
+  struct Sample {
+    const Reflector& reflector;
+    Vector direction;
+    bool transmitted;
+    bool specular;
+    std::optional<visual_t> pdf;
+  };
+
+  virtual std::optional<Sample> SampleAll(
+      const Vector& incoming, const Vector& surface_normal,
+      const Vector& shading_normal, Random& rng,
+      SpectralAllocator& allocator) const = 0;
+
+  virtual std::optional<Sample> SampleDiffuse(
+      const Vector& incoming, const Vector& surface_normal,
+      const Vector& shading_normal, Random& rng,
+      SpectralAllocator& allocator) const = 0;
+
+  struct Reflectance {
+    const Reflector& reflector;
+    Vector direction;
+    bool transmitted;
+    visual_t pdf;
+  };
+
+  virtual std::optional<Reflectance> ReflectanceAll(
+      const Vector& incoming, const Vector& surface_normal,
+      const Vector& shading_normal, const Vector& outgoing,
+      SpectralAllocator& allocator) const = 0;
+
+  virtual std::optional<Reflectance> ReflectanceDiffuse(
+      const Vector& incoming, const Vector& surface_normal,
+      const Vector& shading_normal, const Vector& outgoing,
+      SpectralAllocator& allocator) const = 0;
+};
 
 }  // namespace iris
 
