4ed4651f685c65024bd7c984d5a60a6be10e6daf
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2022-10-29 16:31:20 -0700

    Remove EmissiveMaterialTester

--- iris/emissive_materials/constant_emissive_material_test.cc
+++ iris/emissive_materials/constant_emissive_material_test.cc
@@ -3,13 +3,12 @@
 #include "googlemock/include/gmock/gmock.h"
 #include "googletest/include/gtest/gtest.h"
 #include "iris/spectra/mock_spectrum.h"
-#include "iris/testing/emissive_material_tester.h"
+#include "iris/testing/spectral_allocator.h"
 
 TEST(ConstantEmissiveMaterialTest, Evaluate) {
   auto spectrum = iris::MakeReferenceCounted<iris::spectra::MockSpectrum>();
   iris::emissive_materials::ConstantEmissiveMaterial material(spectrum);
-
-  iris::testing::EmissiveMaterialTester tester;
   EXPECT_EQ(spectrum.Get(),
-            tester.Evaluate(material, iris::TextureCoordinates{}));
+            material.Evaluate(iris::TextureCoordinates{},
+                              iris::testing::GetSpectralAllocator()));
 }
