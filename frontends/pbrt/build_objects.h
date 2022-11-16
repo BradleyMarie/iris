f7e28450c256a561692c54b485194a1c4cb95161
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2022-11-16 03:11:06 -0800

    Add ObjectBuilder

---
+++ frontends/pbrt/build_objects.h
@@ -0,0 +1,37 @@
+#ifndef _FRONTENDS_PBRT_BUILD_OBJECTS_
+#define _FRONTENDS_PBRT_BUILD_OBJECTS_
+
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "frontends/pbrt/object_builder.h"
+#include "frontends/pbrt/parameter_list.h"
+#include "frontends/pbrt/spectrum_manager.h"
+#include "frontends/pbrt/texture_manager.h"
+#include "frontends/pbrt/tokenizer.h"
+
+namespace iris::pbrt_frontend {
+
+template <typename T, typename... BuildArgs>
+T BuildObject(const ObjectBuilder<T, BuildArgs...> builder,
+              Tokenizer& tokenizer, SpectrumManager& spectrum_manager,
+              TextureManager& texture_manager, BuildArgs... build_args) {
+  std::unordered_set<std::string_view> parameters_parsed;
+  std::unordered_map<std::string_view, Parameter> parameters;
+
+  ParameterList parameter_list;
+  while (parameter_list.ParseFrom(tokenizer)) {
+    auto parameter = builder
+                         .Parse(parameter_list, spectrum_manager,
+                                texture_manager, parameters_parsed)
+                         .value();
+    parameters[parameter.GetName()] = parameter;
+  }
+
+  return builder.Build(parameters, build_args);
+}
+
+}  // namespace iris::pbrt_frontend
+
+#endif  // _FRONTENDS_PBRT_BUILD_OBJECTS_
