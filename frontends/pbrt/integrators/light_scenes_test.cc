f793a9378018abc717c90145a5893bd404f10e37
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2022-11-28 00:24:36 -0800

    Add light scenes test

---
+++ frontends/pbrt/integrators/light_scenes_test.cc
@@ -0,0 +1,21 @@
+#include "frontends/pbrt/integrators/light_scenes.h"
+
+#include "googletest/include/gtest/gtest.h"
+
+TEST(ParseLightScene, InvalidType) {
+  EXPECT_EXIT(
+      iris::pbrt_frontend::integrators::ParseLightScene("NotAThing"),
+      testing::ExitedWithCode(EXIT_FAILURE),
+      "ERROR: Unsupported value for parameter lightsamplestrategy: NotAThing");
+}
+
+TEST(ParseLightScene, All) {
+  auto light_scene = iris::pbrt_frontend::integrators::ParseLightScene("all");
+  EXPECT_NE(nullptr, light_scene.get());
+}
+
+TEST(ParseLightScene, Uniform) {
+  auto light_scene =
+      iris::pbrt_frontend::integrators::ParseLightScene("uniform");
+  EXPECT_NE(nullptr, light_scene.get());
+}
