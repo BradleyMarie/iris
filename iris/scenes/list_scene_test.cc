43737ee3194225f8c533370377b1ddaa7cbd2b40
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2023-02-19 21:01:24 -0800

    Remove Scene test suite

--- iris/scenes/list_scene_test.cc
+++ iris/scenes/list_scene_test.cc
@@ -1,9 +1,54 @@
 #include "iris/scenes/list_scene.h"
 
 #include "googletest/include/gtest/gtest.h"
-#include "iris/testing/scene.h"
+#include "iris/geometry/mock_geometry.h"
+#include "iris/reference_counted.h"
+#include "iris/scene_objects.h"
+#include "iris/testing/intersector.h"
 
-TEST(ListSceneTest, SceneTestSuite) {
-  auto builder = iris::scenes::ListScene::Builder::Create();
-  iris::testing::SceneTestSuite(*builder);
+static const iris::face_t kFaces[] = {0u};
+
+TEST(ListScene, TestsAll) {
+  auto mock_geometry0 =
+      iris::MakeReferenceCounted<iris::geometry::MockGeometry>();
+  iris::BoundingBox bounds0(iris::Point(0.0, 0.0, 0.0),
+                            iris::Point(0.5, 0.5, 1.0));
+  EXPECT_CALL(*mock_geometry0, GetFaces())
+      .WillRepeatedly(testing::Return(std::span{kFaces}));
+  EXPECT_CALL(*mock_geometry0, IsEmissive(testing::_))
+      .WillRepeatedly(testing::Return(false));
+  EXPECT_CALL(*mock_geometry0, ComputeBounds(iris::Matrix::Identity()))
+      .WillRepeatedly(testing::Return(bounds0));
+
+  auto mock_geometry1 =
+      iris::MakeReferenceCounted<iris::geometry::MockGeometry>();
+  iris::BoundingBox bounds1(iris::Point(2.0, 2.0, 3.0),
+                            iris::Point(2.5, 2.5, 4.0));
+  EXPECT_CALL(*mock_geometry1, GetFaces())
+      .WillRepeatedly(testing::Return(std::span{kFaces}));
+  EXPECT_CALL(*mock_geometry1, IsEmissive(testing::_))
+      .WillRepeatedly(testing::Return(false));
+  EXPECT_CALL(*mock_geometry1, ComputeBounds(iris::Matrix::Identity()))
+      .WillRepeatedly(testing::Return(bounds1));
+
+  iris::SceneObjects::Builder scene_objects_builder;
+  scene_objects_builder.Add(mock_geometry0);
+  scene_objects_builder.Add(mock_geometry1);
+  auto scene_objects = scene_objects_builder.Build();
+
+  EXPECT_CALL(*mock_geometry0, Trace(testing::_, testing::_))
+      .Times(1)
+      .WillOnce(testing::Return(nullptr));
+  EXPECT_CALL(*mock_geometry1, Trace(testing::_, testing::_))
+      .Times(1)
+      .WillOnce(testing::Return(nullptr));
+
+  iris::Ray ray(iris::Point(0.25, 0.25, -5.5), iris::Vector(0.0, 0.0, -1.0));
+  iris::Hit* closest_hit = nullptr;
+  auto intersector =
+      iris::testing::MakeIntersector(ray, 0.0, 100.0, closest_hit);
+
+  auto scene =
+      iris::scenes::ListScene::Builder().Create()->Build(scene_objects);
+  scene->Trace(ray, intersector);
 }
