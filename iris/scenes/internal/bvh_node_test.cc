de44301a21cdbc453ebfc9993549268054b5a549
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2023-07-24 02:15:21 -0700

    Update BVH node test

--- iris/scenes/internal/bvh_node_test.cc
+++ iris/scenes/internal/bvh_node_test.cc
@@ -14,13 +14,14 @@ TEST(BVHNodeTest, LeafNode) {
 TEST(BVHNodeTest, InteriorNode) {
   iris::BoundingBox bounds(iris::Point(0.0, 0.0, 0.0),
                            iris::Point(1.0, 1.0, 1.0));
-  auto node = iris::scenes::internal::BVHNode::MakeInteriorNode(
-      bounds, iris::Vector::X_AXIS);
-  node.SetRightChildOffset(30);
-  ASSERT_TRUE(node.HasChildren());
-  EXPECT_EQ(&node + 1, &node.LeftChild());
-  EXPECT_EQ(&node + 30, &node.RightChild());
-  EXPECT_EQ(iris::Vector::X_AXIS, node.Axis());
+  std::vector<iris::scenes::internal::BVHNode> nodes(
+      60, iris::scenes::internal::BVHNode::MakeInteriorNode(
+              bounds, iris::Vector::X_AXIS));
+  nodes[0].SetRightChildOffset(30);
+  ASSERT_TRUE(nodes[0].HasChildren());
+  EXPECT_EQ(&nodes[0] + 1, &nodes[0].LeftChild());
+  EXPECT_EQ(&nodes[0] + 30, &nodes[0].RightChild());
+  EXPECT_EQ(iris::Vector::X_AXIS, nodes[0].Axis());
 }
 
 TEST(BVHNodeTest, Intersects) {
