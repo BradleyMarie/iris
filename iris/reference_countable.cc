f81ccfab5cf5cb73b529b31fdc16575c2e086e1b
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2024-07-19 00:33:30 -0700

    Use relaxed memory ordering for reference counting

--- iris/reference_countable.cc
+++ iris/reference_countable.cc
@@ -7,12 +7,12 @@
 namespace iris {
 
 void ReferenceCountable::Increment() noexcept {
-  auto old_value = reference_count_.fetch_add(1u);
+  auto old_value = reference_count_.fetch_add(1u, std::memory_order_relaxed);
   assert(old_value != std::numeric_limits<reference_count_t>::max());
 }
 
 bool ReferenceCountable::Decrement() noexcept {
-  auto old_value = reference_count_.fetch_sub(1u);
+  auto old_value = reference_count_.fetch_sub(1u, std::memory_order_relaxed);
   assert(old_value != 0);
   return old_value == 1;
 }
