606260d375395e03ebab9bf7c8a1b311bbb3609f
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2025-05-31 15:15:18 -0700

    Merge Fresnel into MicrofacetBxdf

--- iris/bxdfs/microfacet_bxdf.cc
+++ iris/bxdfs/microfacet_bxdf.cc
@@ -227,5 +227,56 @@ geometric_t MicrofacetBtdf::ReversedRelativeRefractiveRatio(
 }
 
 }  // namespace internal
+
+const Reflector* FresnelDielectric::AttenuateReflectance(
+    const Reflector& reflectance, visual_t cos_theta_incident,
+    SpectralAllocator& allocator) const {
+  geometric_t eta_incident =
+      std::signbit(cos_theta_incident) ? eta_back_ : eta_front_;
+  geometric_t eta_transmitted =
+      std::signbit(cos_theta_incident) ? eta_front_ : eta_back_;
+  visual_t fresnel_reflectance = internal::FesnelDielectricReflectance(
+      cos_theta_incident, eta_incident, eta_transmitted);
+  return allocator.Scale(&reflectance, fresnel_reflectance);
+}
+
+const Reflector* FresnelDielectric::AttenuateTransmittance(
+    const Reflector& transmittance, visual_t cos_theta_incident,
+    SpectralAllocator& allocator) const {
+  geometric_t eta_incident =
+      std::signbit(cos_theta_incident) ? eta_back_ : eta_front_;
+  geometric_t eta_transmitted =
+      std::signbit(cos_theta_incident) ? eta_front_ : eta_back_;
+  visual_t fresnel_reflectance = internal::FesnelDielectricReflectance(
+      cos_theta_incident, eta_incident, eta_transmitted);
+  return allocator.Scale(&transmittance,
+                         static_cast<visual_t>(1.0) - fresnel_reflectance);
+}
+
+bool FresnelDielectric::IsValid() const {
+  return std::isfinite(eta_front_) &&
+         eta_front_ >= static_cast<visual_t>(1.0) && std::isfinite(eta_back_) &&
+         eta_back_ >= static_cast<visual_t>(1.0);
+}
+
+const Reflector* FresnelConductor::AttenuateReflectance(
+    const Reflector& reflectance, visual_t cos_theta_incident,
+    SpectralAllocator& allocator) const {
+  const Reflector* fresnel = allocator.FresnelConductor(
+      eta_dielectric_, eta_conductor_, k_conductor_, cos_theta_incident);
+  return allocator.Scale(&reflectance, fresnel);
+}
+
+const Reflector* FresnelConductor::AttenuateTransmittance(
+    const Reflector& transmittance, visual_t cos_theta_incident,
+    SpectralAllocator& allocator) const {
+  return nullptr;
+}
+
+bool FresnelConductor::IsValid() const {
+  return std::isfinite(eta_dielectric_) &&
+         eta_dielectric_ > static_cast<visual_t>(0.0) && eta_conductor_;
+}
+
 }  // namespace bxdfs
 }  // namespace iris
--- iris/bxdfs/microfacet_bxdf.h
+++ iris/bxdfs/microfacet_bxdf.h
@@ -6,7 +6,6 @@
 
 #include "iris/bxdf.h"
 #include "iris/bxdf_allocator.h"
-#include "iris/bxdfs/fresnel.h"
 #include "iris/bxdfs/helpers/diffuse_bxdf.h"
 #include "iris/bxdfs/microfacet_distribution.h"
 #include "iris/float.h"
@@ -17,6 +16,58 @@
 
 namespace iris {
 namespace bxdfs {
+
+class Fresnel {
+ public:
+  virtual const Reflector* AttenuateReflectance(
+      const Reflector& reflectance, visual_t cos_theta_incident,
+      SpectralAllocator& allocator) const = 0;
+  virtual const Reflector* AttenuateTransmittance(
+      const Reflector& transmittance, visual_t cos_theta_incident,
+      SpectralAllocator& allocator) const = 0;
+  virtual bool IsValid() const = 0;
+};
+
+class FresnelDielectric final : public Fresnel {
+ public:
+  FresnelDielectric(visual_t eta_front, visual_t eta_back)
+      : eta_front_(eta_front), eta_back_(eta_back) {}
+
+  const Reflector* AttenuateReflectance(
+      const Reflector& reflectance, visual_t cos_theta_incident,
+      SpectralAllocator& allocator) const override;
+  const Reflector* AttenuateTransmittance(
+      const Reflector& transmittance, visual_t cos_theta_incident,
+      SpectralAllocator& allocator) const override;
+  bool IsValid() const override;
+
+ private:
+  visual_t eta_front_;
+  visual_t eta_back_;
+};
+
+class FresnelConductor final : public Fresnel {
+ public:
+  FresnelConductor(visual_t eta_dielectric, const Spectrum* eta_conductor,
+                   const Spectrum* k_conductor)
+      : eta_dielectric_(eta_dielectric),
+        eta_conductor_(eta_conductor),
+        k_conductor_(k_conductor) {}
+
+  const Reflector* AttenuateReflectance(
+      const Reflector& reflectance, visual_t cos_theta_incident,
+      SpectralAllocator& allocator) const override;
+  const Reflector* AttenuateTransmittance(
+      const Reflector& transmittance, visual_t cos_theta_incident,
+      SpectralAllocator& allocator) const override;
+  bool IsValid() const override;
+
+ private:
+  const visual_t eta_dielectric_;
+  const Spectrum* eta_conductor_;
+  const Spectrum* k_conductor_;
+};
+
 namespace internal {
 
 class MicrofacetBrdf final : public helpers::DiffuseBxdf {
