b8660e89a222523d606c8ab9326e3ce0d05d8c6e
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2025-07-10 01:57:04 -0700

    Remove unused data

--- third_party/pbrt/sobolmatrices.h
+++ third_party/pbrt/sobolmatrices.h
@@ -10,10 +10,7 @@
 namespace pbrt {
 
 // Sobol Matrix Declarations
-static constexpr int NSobolDimensions = 1024;
 static constexpr int SobolMatrixSize = 52;
-extern const uint32_t SobolMatrices32[NSobolDimensions * SobolMatrixSize];
-
 static constexpr int VdCSobolMatricesSize = 25;
 static constexpr int VdCSobolMatricesInvSize = 26;
 extern const uint64_t VdCSobolMatrices[VdCSobolMatricesSize][SobolMatrixSize];
